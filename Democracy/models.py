d66eed9408f94435924da8e067f9f5fa5d4299a6
Anuj2001 <2018034@iiitdmj.ac.in>
2020-04-08 13:43:52 +0530

    Updated Files

--- Democracy/models.py
+++ Democracy/models.py
@@ -9,20 +9,6 @@ class Custom_User_Model(AbstractUser):
         return self.username
 
 
-'''class Vote(models.Model):
-    user_id = models.IntegerField(unique=True, default=0)
-    yes = models.IntegerField(default=0)
-    no = models.IntegerField(default=0)
-    do_not_know = models.IntegerField(default=0)
-
-
-
-class Laws(models.Model):
-    law_code = models.IntegerField(null=True)
-    user = models.ForeignKey(Custom_User_Model, on_delete=models.CASCADE)
-'''
-
-
 class Opinion(models.Model):
     username=models.ForeignKey(Custom_User_Model,on_delete=models.CASCADE)
     code_id=models.CharField(max_length=100)
